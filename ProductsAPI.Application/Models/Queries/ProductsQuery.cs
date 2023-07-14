284757fa201d3b08d0230e007c74c6db6da5d806
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-14 19:15:35 -0300

    add products query and web reference to application

---
+++ ProductsAPI.Application/Models/Queries/ProductsQuery.cs
@@ -0,0 +1,12 @@
+namespace ProductsAPI.Application.Models.Queries;
+
+public class ProductsQuery
+{
+    public Guid? Id { get; set; }
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+    public int? Quantity { get; set; }
+    public decimal? Total => Price * Quantity;
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}

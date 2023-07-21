d5ec05a7cf201ce4a6ed389408695cdd92f21f8f
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-21 19:11:28 -0300

    implementing base domain service

--- ProductsAPI.Domain/Services/BaseDomainService.cs
+++ ProductsAPI.Domain/Services/BaseDomainService.cs
@@ -1,6 +1,39 @@
-﻿namespace ProductsAPI.Domain.Services;
+﻿using ProductsAPI.Domain.Contracts.Repositories;
+using ProductsAPI.Domain.Contracts.Services;
 
-public class BaseDomainService
+namespace ProductsAPI.Domain.Services;
+
+public abstract class BaseDomainService<TEntity, TKey> : IBaseDomainService<TEntity, TKey>
+    where TEntity : class
 {
-    
+    private readonly IBaseRepository<TEntity, TKey>? _repository;
+
+    protected BaseDomainService(IBaseRepository<TEntity, TKey>? repository)
+    {
+        _repository = repository;
+    }
+
+    public virtual void Add(TEntity entity)
+    {
+        _repository?.Add(entity);
+        _repository?.SaveChanges();
+    }
+
+    public virtual void Update(TEntity entity)
+    {
+        _repository?.Update(entity);
+        _repository?.SaveChanges();
+    }
+
+    public virtual void Delete(TEntity entity)
+    {
+        _repository?.Delete(entity);
+        _repository?.SaveChanges();
+    }
+
+    public virtual List<TEntity> GetAll() => _repository?.FindAll();
+
+    public virtual TEntity GetById(TKey id) => _repository?.Find(id);
+
+    public virtual void Dispose() => _repository?.Dispose();
 }

4654201de6755d93dd93dd12094ac5a7e90d4817
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-21 13:34:27 -0300

    add product store contract

---
+++ ProductsAPI.Application/Contracts/Stores/IProductStore.cs
@@ -0,0 +1,12 @@
+﻿using ProductsAPI.Application.Models.Queries;
+
+namespace ProductsAPI.Application.Contracts.Stores;
+
+public interface IProductStore
+{
+    void Add(ProductsQuery item);
+    void Update(ProductsQuery item);
+    void Delete(Guid id);
+    List<ProductsQuery> FindAll();
+    ProductsQuery FindById(Guid id);
+}

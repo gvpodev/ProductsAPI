3d5d965472cf8a613c6d65a762f97ff6946c3508
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-21 16:33:07 -0300

    add mongo context and configs

---
+++ ProductsAPI.Infra.Data.MongoDB/Configurations/MongoDbConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductsAPI.Infra.Data.MongoDB.Configurations;
+
+public class MongoDbConfiguration
+{
+    public string? Host { get; set; }
+    public string? Name { get; set; }
+    public bool IsSsl { get; set; }
+}
---
+++ ProductsAPI.Infra.Data.MongoDB/Contexts/MongDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+using ProductsAPI.Infra.Data.MongoDB.Configurations;
+
+namespace ProductsAPI.Infra.Data.MongoDB.Contexts;
+
+public class MongDbContext
+{
+    private readonly MongoDbConfiguration? _configuration;
+
+    public MongDbContext(IOptions<MongoDbConfiguration>? configuration)
+    {
+        _configuration = configuration?.Value;
+    }
+}

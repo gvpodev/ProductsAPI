b1ec1ff1e3619fde5bcd37b348124630b78323cb
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-21 13:26:11 -0300

    implementing unit of work

--- ProductsAPI.Infra.Data.SqlServer/Repositories/UnitOfWork.cs
+++ ProductsAPI.Infra.Data.SqlServer/Repositories/UnitOfWork.cs
@@ -1,6 +1,26 @@
-﻿namespace ProductsAPI.Infra.Data.SqlServer.Repositories;
+﻿using ProductsAPI.Domain.Contracts.Repositories;
+using ProductsAPI.Infra.Data.SqlServer.Contexts;
 
-public class UnitOfWork
+namespace ProductsAPI.Infra.Data.SqlServer.Repositories;
+
+public class UnitOfWork : IUnitOfWork
 {
+    private readonly DataContext? _context;
+
+    public UnitOfWork(DataContext? context)
+    {
+        _context = context;
+    }
+
+    public IProductRepository ProductRepository => new ProductRepository(_context);
     
+    public void SaveChanges()
+    {
+        _context?.SaveChanges();
+    }
+    
+    public void Dispose()
+    {
+        _context?.Dispose();
+    }
 }

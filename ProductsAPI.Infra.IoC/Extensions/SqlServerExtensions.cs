0440c59ffd55f0dbbf059d4e86c688166c2cd8a6
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-21 17:42:27 -0300

    add sql server config

---
+++ ProductsAPI.Infra.IoC/Extensions/SqlServerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductsAPI.Infra.Data.SqlServer.Contexts;
+
+namespace ProductsAPI.Infra.IoC.Extensions;
+
+public static class SqlServerExtensions
+{
+    public static IServiceCollection AddSqlServer(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ProductsAPI");
+
+        services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+        
+        return services;
+    }
+}

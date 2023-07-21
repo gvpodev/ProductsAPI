f621d79be6c9b52c0f695ed34b40f1c1d2c52e8d
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-21 13:07:49 -0300

    add data context

---
+++ ProductsAPI.Infra.Data.SqlServer/Contexts/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Domain.Entities;
+using ProductsAPI.Infra.Data.SqlServer.Configurations;
+
+namespace ProductsAPI.Infra.Data.SqlServer.Contexts;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+    }
+    
+    public DbSet<Product> Products { get; set; }
+}

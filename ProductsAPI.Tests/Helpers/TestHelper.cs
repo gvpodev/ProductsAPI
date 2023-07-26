480c5efafc1358c51f83c12e4ce0681f171395b7
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2023-07-26 19:49:26 -0300

    add api ref to test and add test helper

---
+++ ProductsAPI.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace ProductsAPI.Tests.Helpers;
+
+public static class TestHelper
+{
+    public static HttpClient CreateClient
+        => new WebApplicationFactory<Program>().CreateClient();
+    
+    public static StringContent CreateContent<TRequest>
+        (TRequest request)
+        => new StringContent
+        (JsonConvert.SerializeObject(request),
+            Encoding.UTF8, "application/json");
+    
+    public static TResponse? ReadResponse<TResponse>
+        (HttpResponseMessage message)
+        => JsonConvert.DeserializeObject<TResponse>
+            (message.Content.ReadAsStringAsync().Result);
+}
